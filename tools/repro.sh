55715ce9cba8b81c02174d2f8d1cfb0fb270c748
psondej <patryk.sondej@grupawp.pl>
2024-04-04 23:20:59 +0200

    fix repro file

--- tools/repro.sh
+++ tools/repro.sh
@@ -7,14 +7,16 @@ dnf install -y git python3 python3-devel || true
 dnf install -y curl || true
 
 PY_VER=$(python3 -c "import sys; print(f'{sys.version_info.major}{sys.version_info.minor}', end='')")
+
 python3 -m venv /tmp/venv/
 /tmp/venv/bin/pip install -U pip
 
-/tmp/venv/bin/pip install --force-reinstall /code/pypidist/pwndbg_gdb-14.2.1-cp$PY_VER-cp$PY_VER-manylinux_2_28_x86_64.whl
+WHEEL_FILE=pwndbg_gdb-14.2.1-cp$PY_VER-cp$PY_VER-manylinux_2_28_x86_64.whl
+curl --output /tmp/venv/$WHEEL_FILE -L https://github.com/pwndbg/pwndbg-gdb/blob/main/pypidist/$WHEEL_FILE
+/tmp/venv/bin/pip install --force-reinstall /tmp/venv/$WHEEL_FILE
 /tmp/venv/bin/pip install git+https://github.com/pwndbg/pwndbg
 
 touch /tmp/venv/.skip-venv
-touch /.skip-venv
 
-curl -L https://raw.githubusercontent.com/pwndbg/pwndbg/dev/gdbinit.py --output gdbinit.py
-/tmp/venv/bin/gdb -q -ex 'source ./gdbinit.py'
+curl --output /tmp/venv/gdbinit.py -L https://raw.githubusercontent.com/pwndbg/pwndbg/dev/gdbinit.py
+/tmp/venv/bin/gdb -q -ex 'source /tmp/venv/gdbinit.py'
