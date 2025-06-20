420f5ce8bd10ff2368b494e2c89360bf1f04964c
Patryk Sondej <patryk.sondej@gmail.com>
2025-06-21 00:28:50 +0200

    gdbserver only linux

--- gdb_for_pwndbg/src/gdb_for_pwndbg/gdbserver.py
+++ gdb_for_pwndbg/src/gdb_for_pwndbg/gdbserver.py
@@ -11,6 +11,10 @@ gdb_server_path = here / pathlib.Path('_vendor/bin/gdbserver')
 
 
 def main():
+    if sys.platform == "darwin":
+        print("gdbserver is not supported on macOS. Use Apple's 'debugserver' instead.")
+        os._exit(1)
+
     envs = os.environ.copy()
     os.execve(str(gdb_server_path), sys.argv, env=envs)
 
