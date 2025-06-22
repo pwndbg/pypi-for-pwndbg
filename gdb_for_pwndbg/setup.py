7939db2fe0b2927610e7303b0efdd17c2d59edee
Patryk Sondej <patryk.sondej@gmail.com>
2025-06-22 23:08:09 +0200

    bump ver

--- gdb_for_pwndbg/setup.py
+++ gdb_for_pwndbg/setup.py
@@ -10,7 +10,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="gdb-for-pwndbg",
-    version="16.2.6",
+    version="16.2.7",
     url="https://github.com/pwndbg/pypi-for-pwndbg",
 
     package_dir={"": "src"},  # Optional
