7689d6d7a331d0dd7c24f39d29f0607075a648db
Patryk Sondej <patryk.sondej@gmail.com>
2025-10-17 15:48:58 +0200

    fix debuginfod target

--- gdb_for_pwndbg/libdebuginfod-zig.nix
+++ gdb_for_pwndbg/libdebuginfod-zig.nix
@@ -5,24 +5,41 @@
   zig_0_15,
 }:
 let
-    zig = if stdenv.hostPlatform.system == "x86_64-darwin" then (zig_0_15.overrideAttrs (old: {
-        meta = old.meta // { broken = false; };
+  zig =
+    if stdenv.hostPlatform.system == "x86_64-darwin" then
+      (zig_0_15.overrideAttrs (old: {
+        meta = old.meta // {
+          broken = false;
+        };
         doCheck = false;
         doInstallCheck = false;
-    })) else zig_0_15;
-in stdenv.mkDerivation {
-    name = "libdebuginfod-zig";
-    version = "0.188";
+      }))
+    else
+      zig_0_15;
 
-    src =  fetchFromGitHub {
-      owner = "pwndbg";
-      repo = "debuginfod-zig";
-      rev = "3c1ea47a45c3e9e891b5c00f53373cd51bd69e45";
-      hash = "sha256-kXW98KHB3gq9Xeo/VEzZ/zW7tVhcRFXKcPOLf/KIVDI=";
-#      hash = lib.fakeHash;
-    };
+  target =
+    if stdenv.hostPlatform.isLinux then
+      "-Dtarget=native-linux-gnu.2.28"
+    else
+      "-Dtarget=native-macos.${stdenv.hostPlatform.darwinSdkVersion}";
+in
+stdenv.mkDerivation {
+  name = "libdebuginfod-zig";
+  version = "0.188";
 
-    nativeBuildInputs = [
-      zig.hook
-    ];
+  src = fetchFromGitHub {
+    owner = "pwndbg";
+    repo = "debuginfod-zig";
+    rev = "3c1ea47a45c3e9e891b5c00f53373cd51bd69e45";
+    hash = "sha256-kXW98KHB3gq9Xeo/VEzZ/zW7tVhcRFXKcPOLf/KIVDI=";
+    #      hash = lib.fakeHash;
+  };
+
+  zigBuildFlags = [
+    target
+  ];
+
+  nativeBuildInputs = [
+    zig.hook
+  ];
 }
