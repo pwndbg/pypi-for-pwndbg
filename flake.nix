eb09b4ad936644d79ee4da391bda3f5fba7628af
Patryk Sondej <patryk.sondej@gmail.com>
2026-06-07 19:49:41 +0200

    bump gdb 17.2.0.post2

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
           pkgs.callPackage ./gdb_for_pwndbg/gdb.nix {
             python3 = pkgs."python${v}";
             version = "17.2";
-            pypiVersion = "17.2.0.post1";
+            pypiVersion = "17.2.0.post2";
             src = pkgs.fetchurl {
               url = "mirror://gnu/gdb/gdb-17.2.tar.xz";
               hash = "sha256-HANsDXLks9H7XJTIhjKt1vnXb018TS6nk8EqnxmjIow=";
